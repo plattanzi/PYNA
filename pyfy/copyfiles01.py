c02fcf6692e32e7cd16f5b91bc279c779a09ccdf
platanzi <plattanzi@users.noreply.github.com>
2021-08-30 20:19:09 +0000

    json files

---
+++ pyfy/copyfiles01.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Coping files on the local system"""
+import shutil
+
+def main():
+    shutil.copyfile('/home/student/mycode/pyfy/ciscoex.json', '/home/student/mycode/pyfy/ciscoex.json.bkup')
+    # that was easy!
+
+main()
+#!/usr/bin/env python3
+"""Coping files on the local system"""
+import shutil
+
+def main():
+    shutil.copyfile('/home/student/mycode/pyfy/ciscoex.json', '/home/student/mycode/pyfy/ciscoex.json.bkup')
+    # that was easy!
+
+main()
+
